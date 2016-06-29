d119e636612b7c9b24e9468440aa7b9829e05bb8
Haochuan Liu <haochuan.liu@gmail.com>
2016-06-29 00:15:15 -0700

    load all messages

--- chat-site/src/actions/index.js
+++ chat-site/src/actions/index.js
@@ -1,16 +1,23 @@
 import * as ActionTypes from '../constants/actionTypes';
+import { MESSAGES } from '../constants/messages';
 
 export function loadMessage() {
     // with redux thunk, in actions you have the access to store.dispatch and store.getState
     return ((dispatch, getState) => {
-        const messages = getState().messages;
-        const nextIndex = messages.length === 0 ? 0 : messages.length - 1;
-        writeMessage(dispatch, nextIndex);
+        const messageLength = MESSAGES.length;
+        let index = 0;
+        while (index < messageLength) {
+            setTimeout(function(index) {
+                return function () {
+                    writeMessage(dispatch, index);
+                }
+            }(index), 2000 * index);
+            index++;
+        }
     });
 }
 
 function writeMessage(dispatch, index) {
-    console.log('write');
     dispatch({
         type: ActionTypes.WRITE_MESSAGE,
         index: index
@@ -21,7 +28,6 @@ function writeMessage(dispatch, index) {
 }
 
 function showMessage(dispatch, index) {
-    console.log('show');
     dispatch({
         type: ActionTypes.SHOW_MESSAGE,
         index: index

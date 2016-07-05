79e6846f22144437d37bd3004410ebbe277ed8c5
Haochuan Liu <haochuan.liu@gmail.com>
2016-07-05 12:09:42 -0700

    change message to component

--- chat-site/src/components/Message/index.js
+++ chat-site/src/components/Message/index.js
@@ -1,39 +1,51 @@
 import React, { Component, PropTypes } from 'react';
 import './style.css';
 
-function Message({ messages = [] }) {
-    return (
-        <div>
-            {
-                messages.map((message, key) => {
-                    let content;
-                    if (message.isLoading === 1) {
-                        content = (
-                            <div className="loading">
-                                <div className="dotWrapper">
-                                    <div className="dot"></div>
-                                    <div className="dot"></div>
-                                    <div className="dot"></div>
-                                </div>
-                            </div>
-                        );
-                    } else {
-                        content = (
-                            <div className="message">
-                                {message.content}
-                            </div>
-                        );
-                    }
-                    return (
-                        <div className="message-wrapper" key={key}>
-                            {content}
-                        </div>
+export class Message extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-                    )
-                })
-            }
-        </div>
-    );
+    componentDidMount() {
+        
+    }
+
+    render() {
+        const { messages } = this.props
+        return (
+            <div>
+                {
+                    messages.map((message, key) => {
+                        let content;
+                        if (message.isLoading === 1) {
+                            content = (
+                                <div className="loading">
+                                    <div className="dotWrapper">
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                        <div className="dot"></div>
+                                    </div>
+                                </div>
+                            );
+                        } else {
+                            content = (
+                                <div className="message">
+                                    {message.content}
+                                </div>
+                            );
+                        }
+                        return (
+                            <div className="message-wrapper" key={key}>
+                                {content}
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 }
+
 
 export default Message

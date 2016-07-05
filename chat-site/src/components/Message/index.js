900d34c0fbdc1f349dc0499731e4b6ecb98924b8
Haochuan Liu <haochuan.liu@gmail.com>
2016-07-05 13:57:09 -0700

    complete animation

--- chat-site/src/components/Message/index.js
+++ chat-site/src/components/Message/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import anime from 'animejs';
 import './style.css';
 
 export class Message extends Component {
@@ -7,7 +8,34 @@ export class Message extends Component {
     }
 
     componentDidMount() {
-        
+        console.log(anime.easings);
+    }
+
+    componentDidUpdate() {
+        const { messages } = this.props;
+        const index = messages.length - 1;
+        let messageWidth = 5;
+        const selectorLoading = '.loading-' + index;
+        const selectorMessage = '.message-' + index;
+        if (messages[index].content) {
+            messageWidth += messages[index].content.length / 10 + 0.7;
+        }
+        anime({
+            targets: [selectorLoading],
+            width: ['5rem', '0rem'],
+            elasticity: 300,
+            direction: 'reverse',
+            duration: 600
+        });
+
+        anime({
+            targets: [selectorMessage],
+            width: [messageWidth + 'rem', '5rem'],
+            elasticity: 0,
+            delay: -100,
+            direction: 'reverse',
+            duration: 1000
+        });
     }
 
     render() {
@@ -16,27 +44,25 @@ export class Message extends Component {
             <div>
                 {
                     messages.map((message, key) => {
-                        let content;
-                        if (message.isLoading === 1) {
-                            content = (
-                                <div className="loading">
+                        let classNameLoading = "loading loading-" + key;
+                        let classNameMessage = "message message-" + key;
+                        if (message.isLoading === 0) {
+                            classNameMessage += ' show';
+                        } else {
+                            classNameLoading += ' show';
+                        }
+                        return (
+                            <div className="message-wrapper" key={key}>
+                                <div className={classNameMessage}>
+                                    {message.content}
+                                </div>
+                                <div className={classNameLoading}>
                                     <div className="dotWrapper">
                                         <div className="dot"></div>
                                         <div className="dot"></div>
                                         <div className="dot"></div>
                                     </div>
                                 </div>
-                            );
-                        } else {
-                            content = (
-                                <div className="message">
-                                    {message.content}
-                                </div>
-                            );
-                        }
-                        return (
-                            <div className="message-wrapper" key={key}>
-                                {content}
                             </div>
 
                         )
--- chat-site/src/constants/messages.js
+++ chat-site/src/constants/messages.js
@@ -1,4 +1,4 @@
 export const MESSAGES = [
     "Hey message 1",
-    "Hey message 2"
+    "Hey message 2 this is a long message"
 ];

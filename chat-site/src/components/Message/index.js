8116093d7139abb35ed45c1be43b256ee829e0f4
Haochuan Liu <haochuan.liu@gmail.com>
2016-07-05 17:35:35 -0700

    finish.

--- chat-site/src/components/Message/index.js
+++ chat-site/src/components/Message/index.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import anime from 'animejs';
 import './style.css';
 
@@ -8,17 +9,22 @@ export class Message extends Component {
     }
 
     componentDidMount() {
-        console.log(anime.easings);
+        // console.log(anime.easings);
     }
 
     componentDidUpdate() {
+        // const fontSize = parseInt(getComputedStyle(document.body, null).getPropertyValue('font-size'));
+        // const ratio = fontSize / 16;
         const { messages } = this.props;
         const index = messages.length - 1;
-        let messageWidth = 5;
+        let messageWidth = 0;
         const selectorLoading = '.loading-' + index;
         const selectorMessage = '.message-' + index;
-        if (messages[index].content) {
-            messageWidth += messages[index].content.length / 10 + 0.7;
+        const selectorContent = '.content-' + index;
+        if (messages[index].content.length) {
+            // messageWidth = messages[index].content.length > 24 ? 55 :  messages[index].content.length / 2;
+            
+            messageWidth = messages[index].content.length
         }
         anime({
             targets: [selectorLoading],
@@ -30,12 +36,20 @@ export class Message extends Component {
 
         anime({
             targets: [selectorMessage],
-            width: [messageWidth + 'rem', '5rem'],
-            elasticity: 0,
-            delay: -100,
+            width: [messageWidth + 'rem', messageWidth + 'rem'],
+            elasticity: 100,
             direction: 'reverse',
-            duration: 1000
+            duration: 1200
         });
+
+        // anime({
+        //     targets: [selectorContent],
+        //     opacity: [0, 1],
+        //     // elasticity: 200,
+        //     // direction: 'reverse',
+        //     easing: 'easeInQuart',
+        //     duration: 1500
+        // });
     }
 
     render() {
@@ -46,6 +60,7 @@ export class Message extends Component {
                     messages.map((message, key) => {
                         let classNameLoading = "loading loading-" + key;
                         let classNameMessage = "message message-" + key;
+                        let classNameContent = "content content-" + key;
                         if (message.isLoading === 0) {
                             classNameMessage += ' show';
                         } else {
@@ -54,7 +69,18 @@ export class Message extends Component {
                         return (
                             <div className="message-wrapper" key={key}>
                                 <div className={classNameMessage}>
-                                    {message.content}
+                                    <div className={classNameContent}>
+                                        {
+                                            message.content.data.map((line, key) => {
+                                                return (
+                                                    <div key={key}>
+                                                        {line.content} 
+                                                        <a href={line.link.url}>{line.link.display}</a>
+                                                    </div>
+                                                );
+                                            })
+                                        }
+                                    </div>
                                 </div>
                                 <div className={classNameLoading}>
                                     <div className="dotWrapper">
--- chat-site/src/constants/messages.js
+++ chat-site/src/constants/messages.js
@@ -1,4 +1,123 @@
 export const MESSAGES = [
-    "Hey message 1",
-    "Hey message 2 this is a long message"
+    {
+        length: 4.6,
+        data: [
+            {
+                content: "Hey there.",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            }
+        ]
+    },
+    {
+        length: 13.5,
+        data: [
+            {
+                content: "I'm Haochuan.",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            },
+            {
+                content: "People prefer call me `H` here.",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            }
+        ]
+    },
+    {
+        length: 16.9,
+        data: [
+            {
+                content: "I write code for web, audio and music",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            },
+            {
+                content: "on ",
+                link: {
+                    display: "my github",
+                    url: "https://github.com/haochuan"
+                }
+            }
+        ]
+    },
+    {
+        length: 11,
+        data: [
+            {
+                content: "I write blogs in ",
+                link: {
+                    display: "my blog,",
+                    url: "http://blog.haochuan.io/"
+                }
+            },
+            {
+                content: " and in ",
+                link: {
+                    display: "medium",
+                    url: "https://medium.com/@haochuan"
+                }
+            }
+        ]
+    },
+    {
+        length: 16.8,
+        data: [
+            {
+                content: "I use vim a lot,",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            },
+            {
+                content: "play guitar for metal on ",
+                link: {
+                    display: "youtube, ",
+                    url: "https://www.youtube.com/channel/UCNESazgvF_NtDAOJrJMNw0g"
+                }
+            },
+            {
+                content: "and spend 2+ hours in gym everyday.",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            }
+        ]
+    },
+    {
+        length: 10.6,
+        data: [
+            {
+                content: "If you want to know more about me, feel free to sent me a note via ",
+                link: {
+                    display: "email",
+                    url: "mailto:haochuan.liu@gmail.com"
+                }
+            }
+        ]
+    },
+    {
+        length: 7.5,
+        data: [
+            {
+                content: "Have a nice day!",
+                link: {
+                    display: "",
+                    url: ""
+                }
+            }
+        ]
+    }
+
+
 ];

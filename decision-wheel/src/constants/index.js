2f6c43ac74db8eadb37bee8533374b893a27ab43
Haochuan Liu <haochuan.liu@gmail.com>
2017-01-03 13:56:44 -0800

    cleanup

--- decision-wheel/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const ADD = 'ADD';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const CHANGE_TAB = 'CHANGE_TAB';
-export const CHANGE_FEILDTYPE = 'CHANGE_FEILDTYPE';
-
